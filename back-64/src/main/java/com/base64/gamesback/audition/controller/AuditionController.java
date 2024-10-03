8a2bd59c4f5b7fd29cee30d1d704d4b494007cec
Sebastian Leon <juanleon@wposs.com>
2024-10-03 08:10:20 -0500

    eliminacion de importaciones

--- back-64/src/main/java/com/base64/gamesback/audition/controller/AuditionController.java
+++ back-64/src/main/java/com/base64/gamesback/audition/controller/AuditionController.java
@@ -2,14 +2,12 @@ package com.base64.gamesback.audition.controller;
 
 import com.base64.gamesback.audition.dto.AuditionResult;
 import com.base64.gamesback.audition.service.AuditionService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 import java.util.Set;
 
 
