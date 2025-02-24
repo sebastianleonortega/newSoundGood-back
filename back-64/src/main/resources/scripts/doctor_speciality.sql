a568a3f64f5f070038542282a9e37a5021a0ebdf
HaroldMartinez <hfmartinezc@ufpso.edu.co>
2025-02-24 10:56:50 -0500

    ajustes scripts

--- back-64/src/main/resources/scripts/doctor_speciality.sql
+++ back-64/src/main/resources/scripts/doctor_speciality.sql
@@ -3,8 +3,8 @@ INSERT INTO main.doctor_speciality (doctor_id, speciality_id) VALUES ('d456712e-
 INSERT INTO main.doctor_speciality (doctor_id, speciality_id) VALUES ('a97b589c-2a89-44c3-8a52-b7d9f52a045e', '7b3a124d-7ae4-486f-8f2c-02ab5654e6c1');
 INSERT INTO main.doctor_speciality (doctor_id, speciality_id) VALUES ('e1f2d3c4-5678-4a90-bcde-123456789abc', 'ce173b92-4bbd-467e-aa02-d2b4e6a4a5a1');
 INSERT INTO main.doctor_speciality (doctor_id, speciality_id) VALUES ('12345678-9012-3456-7890-abcdef123456', '9e54e1f8-6231-4eab-8d86-df8f45c064a7');
-INSERT INTO main.doctor_speciality (doctor_id, speciality_id) VALUES ('09876543-2109-8765-4321-fedcba098765', 'b8445a2c-9818-44e0-95fd-3d04215392f2');
-INSERT INTO main.doctor_speciality (doctor_id, speciality_id) VALUES ('a1b2c3d4-5678-90ab-cdef-0123456789ab', '87b95145-d527-45bf-9d19-80e675b61282');
-INSERT INTO main.doctor_speciality (doctor_id, speciality_id) VALUES ('b1a2c3d4-5678-90ab-cdef-0123456789ab', 'bc429963-4871-4db9-8406-f4d054f1bb41');
-INSERT INTO main.doctor_speciality (doctor_id, speciality_id) VALUES ('c1a2b3d4-5678-90ab-cdef-0123456789ab', '434aea46-7a83-4efe-b777-121488ae7058');
-INSERT INTO main.doctor_speciality (doctor_id, speciality_id) VALUES ('d1a2b3c4-5678-90ab-cdef-0123456789ab', '985b9cb0-5bcc-410d-bb3c-365c2395fdc5');
+-- INSERT INTO main.doctor_speciality (doctor_id, speciality_id) VALUES ('09876543-2109-8765-4321-fedcba098765', 'b8445a2c-9818-44e0-95fd-3d04215392f2');
+-- INSERT INTO main.doctor_speciality (doctor_id, speciality_id) VALUES ('a1b2c3d4-5678-90ab-cdef-0123456789ab', '87b95145-d527-45bf-9d19-80e675b61282');
+-- INSERT INTO main.doctor_speciality (doctor_id, speciality_id) VALUES ('b1a2c3d4-5678-90ab-cdef-0123456789ab', 'bc429963-4871-4db9-8406-f4d054f1bb41');
+-- INSERT INTO main.doctor_speciality (doctor_id, speciality_id) VALUES ('c1a2b3d4-5678-90ab-cdef-0123456789ab', '434aea46-7a83-4efe-b777-121488ae7058');
+-- INSERT INTO main.doctor_speciality (doctor_id, speciality_id) VALUES ('d1a2b3c4-5678-90ab-cdef-0123456789ab', '985b9cb0-5bcc-410d-bb3c-365c2395fdc5');
